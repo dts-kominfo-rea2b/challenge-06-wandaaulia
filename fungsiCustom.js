14245f7c72c28f0dc63d95da9f34aeb1da0d9ba4
wanda <wandaul16@gmail.com>
2022-06-11 21:48:30 +0700

    first commit

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,43 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  const arrayData = [];
+
+  fs.readFile(file1, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    }
+
+    let getData = JSON.parse(data);
+    let getMessage = getData.message.split(" ");
+    arrayData.push(getMessage[1]);
+
+
+  fs.readFile(file2, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    }
+
+   let getData = JSON.parse(data);
+    let getMessage = getData[0].message.split(" ");
+    arrayData.push(getMessage[1]);
+ 
+
+   fs.readFile(file3, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    }
+
+  let getData = JSON.parse(data);
+    let getMessage = getData[0].data.message.split(" ");
+    arrayData.push(getMessage[1]);
+
+  return fnCallback(err, arrayData);
+    });
+     });
+    });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
